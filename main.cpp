a25f5aea8172539219060343895dd9020798a368
John <dazajohn08@gmail.com>
2026-03-25 20:43:40 -0700

    done 2

--- main.cpp
+++ main.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int main()
 {
-	int begin, end;
-	do
-	{
-		cout << "Enter your range [from, to] \n";
-		cin >> begin >> end;
-	} while (begin >= end || begin < 2 || end < 0);
+    int begin, end;
+    do
+    {
+        cout << "Enter your range [from, to] \n";
+        cin >> begin >> end;
+    } while (begin >= end || begin < 2 || end < 0);
 
-	for(int num=begin; num <= end; num++){
-	int i;
-	for(int i = 2; i < num; i++){
-		if(num % i == 0)
-		break;
-	}
-	if(i == num)
-		cout << num << " is prime" << endl;
-}
-cout << endl;
-return 0;
+    for(int num=begin; num <= end; num++){
+        int i;
+        for(i = 2; i < num; i++){
+            if(num % i == 0)
+                break;
+        }
+        if(i == num)
+            cout << num << " is prime" << endl;
+    }
+    cout << endl;
+    return 0;
 }
